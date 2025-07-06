2dd88647459dac13803ef1afde60099f068df07e
Narsimha reddy <manjulareddy0907@gmail.com>
2025-07-06 15:55:10 +0530

    Describe changes

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,60 @@
-from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Post
 
-# Create your views here.
+
+# ✅ LIST VIEW
 class PostListView(ListView):
     model = Post
     template_name = 'post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']  # optional: newest first
 
+
+# ✅ DETAIL VIEW
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+# ✅ CREATE VIEW
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_form.html'
+    fields = ['title', 'body']
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+# ✅ UPDATE VIEW
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'post_form.html'
+    fields = ['title', 'body']
+    success_url = reverse_lazy('post_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+
+# ✅ DELETE VIEW
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
